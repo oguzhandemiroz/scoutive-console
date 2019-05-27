8c9279d0376e6fabd14bd1e5aced9fba4e74e002
Oguzhan Demiroz <oguzhan.demiroz@gmail.com>
2019-05-27 19:02:29 +0300

    router config

--- src/layouts/Core.jsx
+++ src/layouts/Core.jsx
@@ -1,52 +1,43 @@
-import React, {Component} from "react";
-import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import indexRoutes from "../routes/index.jsx";
-import {createBrowserHistory} from "history";
+import { createBrowserHistory } from "history";
 import Protected from "../components/Auth/Login.jsx";
 
 const history = createBrowserHistory();
 const auth = {
-    isAuthenticated: false,
-    authenticated(callback) {
-        this.isAuthenticated = true;
-        setTimeout(callback, 100); // fake async
-    },
-    signout(callback) {
-        this.isAuthenticated = false;
-        setTimeout(callback, 100);
-    }
+	isAuthenticated: false,
+	authenticated(callback) {
+		this.isAuthenticated = true;
+		setTimeout(callback, 100); // fake async
+	},
+	signout(callback) {
+		this.isAuthenticated = false;
+		setTimeout(callback, 100);
+	}
 };
 
-const PrivateRoute = ({component: Component, ...rest}) => (
-    <Route
-        {...rest}
-        render={props =>
-            auth.isAuthenticated === true ? <Component {...props} /> : <Redirect to="/" />
-        }
-    />
+const PrivateRoute = ({ component: Component, ...rest }) => (
+	<Route
+		{...rest}
+		render={props => (auth.isAuthenticated === true ? <Component {...props} /> : <Redirect to="/" />)}
+	/>
 );
 
 class Core extends Component {
-    render() {
-        return (
-            <Router history={history}>
-                <Switch>
-                    {indexRoutes.map((route, key) => {
-                        console.log(route);
-                        return (
-                            <Route
-                                {...route.props}
-                                key={key}
-                                path={route.path}
-                                component={route.component}
-                            />
-                        );
-                    })}
-                    <PrivateRoute path="/protected" component={Protected} />
-                </Switch>
-            </Router>
-        );
-    }
+	render() {
+		return (
+			<Router history={history}>
+				<Switch>
+					<PrivateRoute exact path="/protected" component={Protected} />
+					{indexRoutes.map((route, key) => {
+						console.log(route);
+						return <Route {...route.props} key={key} path={route.path} component={route.component} />;
+					})}
+				</Switch>
+			</Router>
+		);
+	}
 }
 
 export default Core;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,22 +3,24 @@ import Dashboard from "../views/Dashboard/Dashboard.jsx";
 import _404 from "../views/Pages/404.jsx";
 
 var indexRoutes = [
-    {
-        path: "/",
-        name: "Login",
-        component: LoginPage,
-        props: {exact: true}
-    },
-    {
-        path: "/app",
-        name: "Dashboard",
-        component: Dashboard,
-        props: {exact: true}
-    },
-    {
-        name: "404",
-        component: _404
-    }
+	{
+		path: "/",
+		name: "Login",
+		component: LoginPage,
+		props: { exact: true }
+	},
+	{
+		path: "/app/dashboard",
+		name: "Dashboard",
+		component: Dashboard,
+		layout: "/app",
+		props: { exact: true, strict: true }
+	},
+	{
+		path: "*",
+		name: "404",
+		component: _404
+	}
 ];
 
 export default indexRoutes;
