6f53083c0baefda1b405549147c8cf57601d8afb
Oguzhan Demiroz <oguzhan.demiroz@gmail.com>
2019-08-28 21:22:04 +0300

    FABs Button Created && Player Fee Bug Fixed

--- src/components/Others/FABs.jsx
+++ src/components/Others/FABs.jsx
@@ -1,8 +1,60 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+const $ = require("jquery");
 
 export class FABs extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			dropdown: true
+		};
+	}
+
+	componentDidMount() {
+		$('[data-toggle="tooltip"]').tooltip({
+			trigger: "hover",
+			delay: { show: "500" }
+		});
+
+		$("#FABs").click(function() {
+			$(".FABs-wrapper").toggleClass("fixed");
+			$('[data-toggle="tooltip"]').tooltip("hide");
+		});
+	}
+
 	render() {
-		return <div className="fixed-bottom">HELLO</div>;
+		return (
+			<div className="FABs-wrapper" data-toggle="tooltip" title="Hızlı Erişim Menüsü">
+				<div className="dropup">
+					<button id="FABs" className="btn btn-dark btn-lg" data-toggle="dropdown">
+						<i className="fe fe-zap"></i>
+					</button>
+
+					<div className="dropdown-menu">
+						<h6 className="dropdown-header">Hızlı Erişim Menüsü</h6>
+						<div className="dropdown-divider"></div>
+						<Link to="/app/players/add" className="dropdown-item">
+							<i className="fa fa-user-plus mr-1"></i> Öğrenci Ekle
+						</Link>
+						<Link to="/app/players/payment" className="dropdown-item">
+							<i className="fa fa-hand-holding-usd mr-1"></i> Ödeme Al
+						</Link>
+					</div>
+				</div>
+
+				{/* <div className={`FABs-dropdown ${dropdown ? "show" : ""}`}>
+					<h5 className="dropdown-header">Hızlı Erişim Menüsü</h5>
+					<div className="dropdown-divider"></div>
+					<Link to="#" className="dropdown-item">
+						<i className="fe fe-user mr-1"></i> Öğrenci Ekle
+					</Link>
+					<Link to="#" className="dropdown-item">
+						<i className="fa fa-hand-holding-usd mr-1"></i> Ödeme Al
+					</Link>
+				</div> */}
+			</div>
+		);
 	}
 }
 
