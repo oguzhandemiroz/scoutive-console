84396621a67a42f0e5a1c2f511c40e63df4cb5a1
Oguzhan Demiroz <oguzhan.demiroz@gmail.com>
2020-03-23 14:52:43 +0300

    Bank Modal Fill Data

--- src/components/Pages/Settings/Billing/BankModal.jsx
+++ src/components/Pages/Settings/Billing/BankModal.jsx
@@ -1,27 +1,95 @@
 import React, { Component } from "react";
-const $ = require("jquery");
 
 export class BankModal extends Component {
-    componentDidMount() {
-        $("#exampleModal").on("show.bs.modal", function(event) {
-            var button = $(event.relatedTarget); // Button that triggered the modal
-            var recipient = button.data("whatever"); // Extract info from data-* attributes
-            // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-            // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-            console.log(recipient);
-        });
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            bank: { detail: { account_name: "" } }
+        };
     }
+
+    componentDidMount() {
+        const { bank } = this.props;
+        this.setState({ bank: bank });
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const { bank } = this.props;
+
+        if (bank !== nextProps.bank) {
+            this.setState({ bank: nextProps.bank });
+        }
+    }
+
     render() {
+        const { bank } = this.state;
+
         return (
             <div
                 className="modal fade"
-                id="exampleModal"
+                id="bankModal"
                 tabIndex="-1"
                 role="dialog"
-                aria-labelledby="exampleModalLabel"
+                aria-labelledby="bankModalLabel"
                 aria-hidden="true">
                 <div className="modal-dialog modal-lg">
-                    <div className="modal-content">...</div>
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">
+                                <i className="fa fa-university mr-1"></i> {bank.name}
+                            </h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close" />
+                        </div>
+                        <div className="modal-body pt-0">
+                            <div className="d-flex justify-content-center align-items-center p-5">
+                                <img src={bank.logo} alt={bank.name} title={bank.name} height="70" />
+                            </div>
+                            <table className="table table-vcenter table-bordered table-hover mb-0">
+                                <tbody>
+                                    <tr>
+                                        <th>Hesap Sahibi</th>
+                                        <td>{bank.detail.owner_name}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Şube Bilgisi</th>
+                                        <td>
+                                            {bank.detail.branch_no} &mdash; {bank.detail.branch_name}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th>Hesap Numarası</th>
+                                        <td>{bank.detail.account_no}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>IBAN</th>
+                                        <td>{bank.detail.iban}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Para Birimi</th>
+                                        <td>{bank.detail.currency}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="alert card-alert m-0 alert-info alert-icon">
+                            <i className="fe fe-info mr-2"></i>
+                            <p>
+                                <strong>Havale/EFT Hakkında Bilgilendirme</strong>
+                            </p>
+                            <p>
+                                Havale, aynı bankanın hesapları arasında yapılan para transferi işlemidir. 7 gün, 24
+                                saat boyunca havale işlemini yapabilirsiniz. Bu işlemler en geç 30 dakika içinde hesaba
+                                aktarılır.
+                            </p>
+                            <p>
+                                EFT, farklı bankalar aracılığıyla da yapılabilmektedir. EFT, yalnızca hafta içi mesai
+                                saatleri içinde yapılmaktadır. Bu işlem saatleri ise bankalar tarafından belirlenerek,
+                                bankadan bankaya değişiklik gösterebilir. EFT saatinden sonra yaptığınız bir para
+                                transferi mesait saatileri içerisinde 08:00’den sonra hesaba ulaşacaktır.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
--- src/components/Pages/Settings/Billing/Banks.jsx
+++ src/components/Pages/Settings/Billing/Banks.jsx
@@ -15,22 +15,61 @@ import kuveytturk from "../../../../assets/images/banks/kuveytturk.svg";
 import is from "../../../../assets/images/banks/is.svg";
 
 const banks = [
-    { id: "0", name: "Garanti BBVA", logo: garanti, active: true },
-    { id: "1", name: "Türk Ekonomi Bankası", logo: teb, active: true },
-    { id: "2", name: "Halkbank", logo: halkbank, active: false },
-    { id: "3", name: "Türkiye İş Bankası", logo: is, active: false },
-    { id: "4", name: "VakıfBank", logo: vakifbank, active: false },
-    { id: "5", name: "Ziraat Bankası", logo: ziraat, active: false },
-    { id: "6", name: "Kuveyt Türk Katılım Bankası", logo: kuveytturk, active: false },
-    { id: "7", name: "QNB Finansbank", logo: qnb, active: false },
-    { id: "8", name: "Yapı Kredi", logo: yapikredi, active: false },
-    { id: "9", name: "Akbank", logo: akbank, active: false },
-    { id: "10", name: "ING", logo: ing, active: false },
-    { id: "11", name: "Albaraka Türk Katılım Bankası", logo: albaraka, active: false }
+    {
+        id: "0",
+        name: "Garanti BBVA",
+        logo: garanti,
+        active: true,
+        detail: {
+            owner_name: "Oğuzhan DEMİRÖZ",
+            account_no: "6295542",
+            branch_no: "135",
+            branch_name: "ORTAKÖY Şubesi",
+            iban: "TR43 0006 2000 1350 0006 2955 42",
+            currency: "TL"
+        }
+    },
+    {
+        id: "1",
+        name: "TEB (Türk Ekonomi Bankası)",
+        logo: teb,
+        active: true,
+        detail: {
+            owner_name: "Oğuzhan DEMİRÖZ",
+            account_no: "33477522",
+            branch_no: "929",
+            branch_name: "CEPTETEB",
+            iban: "TR49 0003 2000 0000 0033 4775 22",
+            currency: "TL"
+        }
+    },
+    { id: "2", name: "Halkbank", logo: halkbank, active: false, detail: null },
+    { id: "3", name: "Türkiye İş Bankası", logo: is, active: false, detail: null },
+    { id: "4", name: "VakıfBank", logo: vakifbank, active: false, detail: null },
+    { id: "5", name: "Ziraat Bankası", logo: ziraat, active: false, detail: null },
+    { id: "6", name: "Kuveyt Türk Katılım Bankası", logo: kuveytturk, active: false, detail: null },
+    { id: "7", name: "QNB Finansbank", logo: qnb, active: false, detail: null },
+    { id: "8", name: "Yapı Kredi", logo: yapikredi, active: false, detail: null },
+    { id: "9", name: "Akbank", logo: akbank, active: false, detail: null },
+    { id: "10", name: "ING", logo: ing, active: false, detail: null },
+    { id: "11", name: "Albaraka Türk Katılım Bankası", logo: albaraka, active: false, detail: null }
 ];
 
 export class Banks extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedBank: { detail: { account_name: "" } }
+        };
+    }
+
+    handleBank = bank => {
+        this.setState({ selectedBank: bank });
+    };
+
     render() {
+        const { selectedBank } = this.state;
         return (
             <div className="card">
                 <div className="card-header">
@@ -42,15 +81,15 @@ export class Banks extends Component {
                     <div className="row gutters-0 bank-group">
                         {banks.map(bank => (
                             <div
-                                data-whatever={bank.name}
-                                data-target="#exampleModal"
+                                onClick={() => this.handleBank(bank)}
+                                data-target="#bankModal"
                                 data-toggle="modal"
                                 className={`col-sm-3 col-4 p-sm-5 p-4 border bank ${bank.active ? "bank-active" : ""}`}
                                 key={bank.id}>
-                                <img src={bank.logo} alt={bank.name} height="35" />
+                                <img src={bank.logo} alt={bank.name} title={bank.name} height="35" />
                             </div>
                         ))}
-                        <BankModal />
+                        <BankModal bank={selectedBank} />
                     </div>
                 </div>
             </div>
